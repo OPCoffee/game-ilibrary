26d6b68aedbd17a249ce2c59e7d5837119af9900
OPCoffee <willmao88@gmail.com>
2023-06-05 20:50:37 -0500

    Add initial files and html skeleton

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Game() {
+    constructor(title, hoursPlayed, completed){
+        this.title = title;
+        this.hoursPlayed = hoursPlayed;
+        this.completed = completed;
+    }
+}
+
+function addGameToLibrary(){
+    
+}
